bd9fceefbf0cfe883266c00d5e9ee39aa4fe9c49
jmredfern <jmredfern1970@gmail.com>
2020-06-24 10:12:43 -0500

    Updated Cat.java with isAMouser mem var, getter.

--- CatProject/src/com/redfern/cat_project/Cat.java
+++ CatProject/src/com/redfern/cat_project/Cat.java
@@ -5,11 +5,16 @@ public class Cat {
 	private String name;
 	private String gender;
 	private int age;
+	private boolean isAMouser = true;
 
 	public Cat(String name, String gender, int age) {
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
+	}
+
+	public boolean isAMouser() {
+		return isAMouser;
 	}
 
 	public String getName() {
